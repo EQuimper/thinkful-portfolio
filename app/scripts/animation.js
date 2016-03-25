b2c82747a53b123cc4fc32380670386299f7f5d1
UrNightmares <quimperemanuel@gmail.com>
2016-03-24 22:43:52 -0600

    Update 2016-03-25T04:43:46.986Z

--- app/scripts/animation.js
+++
@@ -1,9 +0,0 @@
-$('.fa').hover(function (){
-  $(this).addClass('animated wobble ease-in-out');
-});
-
-$(".triangle").click(function () {
-  var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-  $(this).velocity("scroll", {duration: 2500, section: ".about_me", easing: "spring", mobileHA: false})
-  .velocity({ opacity: 1 });
-});
